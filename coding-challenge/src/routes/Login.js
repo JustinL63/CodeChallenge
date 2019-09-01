ee8c0a040f7f8389f8e3c2469f702670d0e0640f
Justin Lamb <lambjg63@gmail.com>
2019-09-01 12:39:47 -0400

    tweaking

--- coding-challenge/src/routes/Login.js
+++ coding-challenge/src/routes/Login.js
@@ -1,32 +1,34 @@
 import React from "react"
+//import Login from "./Login.css"
 
-class Login extends React.Component {
-    constructor() {
-        super()
-            this.setState=({
-                user: [
-                    {userName: "", email: "", password: "", admin: false }
-                ]
-            })
+const Login =(props) => {
+    const style = {
+        display: "inline-block",
+        padding: "16px",
+        margin: "16px",
+        border: "1px solid black",
+        textAlign: "center"
 
     }
-    render() {
         return(
-            <div>
+            <div className="Login" style={style}>
                 <form>
-                    <input type="text" name="userName" placeholder="User Name"/>
+                    <input type="text" name="firstName" placeholder="First Name"/>
+                    <br/>
+                    <input type="text" name="lastName" placeholder="Last Name"/>
                     <br/>
                     <input type="text" name="email" placeholder="Email"/>
                     <br/>
                     <input type="text" name="password" placeholder="Password"/>
                     <br/>
-                    <p>Are You an Administrator?</p>
-                    <input type="checkbox" name="admin"/>
+                    <p>Are You an Administrator?</p><input type="checkbox" name="admin"/>
+                    <br/>
+                    <button>Submit</button>
                 </form>
 
             </div>
         )
     }
-}
+
 
 export default Login
